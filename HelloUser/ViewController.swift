8d817182a6ed0d141ada2f34cd4bd6c00d81e186
Ilya <fekl13in@gmail.com>
2025-03-05 17:53:49 +0300

    Initial Commit

---
+++ HelloUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloUser
+//
+//  Created by Ilya Feklin on 05.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
